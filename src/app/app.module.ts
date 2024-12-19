c29976451a293d50717933f683c688018d17aa79
Giacomo Rossettini <rossegiac@gmail.com>
2024-12-19 13:32:55 +0100

    btn funziona!!!!!!!!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JackBtnsComponent } from './jack-btns/jack-btns.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JackBtnsComponent
   ],
   imports: [
     BrowserModule,
