c9f9e96f5210f3fbc979d33e89820d2f366ebdb4
Giacomo Rossettini <rossegiac@gmail.com>
2024-12-19 11:41:24 +0100

    start

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        nightblue: "#1E293B",
+        watergreen: "#22C55E",
+        secondarygray: "#F3F4F6",
+        coralrose: "#FB7185",
+      },
+    },
+  },
+  plugins: [],
+};
