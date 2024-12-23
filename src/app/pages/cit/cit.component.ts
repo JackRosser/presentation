74d675510d56a4081fce772c6218bee73b4c8d6e
Giacomo Rossettini <rossegiac@gmail.com>
2024-12-23 11:16:41 +0100

    appear funziona

--- src/app/pages/cit/cit.component.ts
+++ src/app/pages/cit/cit.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cit',
   templateUrl: './cit.component.html',
   styleUrls: ['./cit.component.scss']
 })
-export class CitComponent {
+export class CitComponent  {
+
+  position:number = 0;
+
+
 
 }
