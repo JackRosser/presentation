7d73fa610522dda3fa458dfb20fa4a0a9da67be0
Giacomo Rossettini <rossegiac@gmail.com>
2024-12-24 13:54:53 +0100

    secondcit ok

--- src/app/pages/secondcit/secondcit.component.ts
+++ src/app/pages/secondcit/secondcit.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-secondcit',
   templateUrl: './secondcit.component.html',
   styleUrl: './secondcit.component.scss'
 })
-export class SecondcitComponent {
+export class SecondcitComponent implements OnInit{
+
+  constructor(private languageSvc:LanguageService) {}
+
+  lineOne!:any
+  lineTwo!:any
+  lineThree!:any
+
+  ngOnInit(): void {
+    this.languageSvc.language$.subscribe(list=> {
+      this.lineOne = list?.scrivere
+      this.lineTwo = list?.maunapassione
+      this.lineThree = list?.ognigiorno
+    })
+  }
 
 }
